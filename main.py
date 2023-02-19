b59b67b0ac6b4fce2802107af095654be46d1c07
ghostenginesio <ghostengines.io@gmail.com>
2023-02-19 09:50:15 +0530

    commit

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from llm import todochat
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Response
+from langchain import OpenAI
+from llm import get_current_datetime
+from os import environ, getenv
 
 app = FastAPI()
 
-origins = ["*"]
-
+# Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
-) 
+)
 
-@app.get('/', include_in_schema= False)
-def home():
-    return {'status': 'Working'}
+if 'OPENAI_API_KEY' in environ:
+    openai_api_key = environ['OPENAI_API_KEY']
+else:
+    from dotenv import load_dotenv
+    load_dotenv()
+    openai_api_key = getenv('OPENAI_API_KEY')
 
-@app.get('/{date}/{task}')
-def function(date, task):
-    return todochat(plan=task, complete_before=date)
+# Initialize OpenAI language model
+llm = OpenAI(model_name="text-davinci-003", best_of=1,  max_tokens=2000, openai_api_key=openai_api_key)
+
+
+@app.get('/')
+async def check():
+    return {'status': 'OK'}
+
+@app.post("/generate")
+async def generate_tasks(plan: str, complete_before: int):
+    prompt = '''Please generate a JSON of subtasks and their deadlines for the task %s. The subtasks should be reasonable and not take more than a day to complete. I want to complete all the subtasks in %s days. Current Timestamp: %s
+    The JSON output should be in the following format: [{"name": Subtask 1, "description": Description of subtask 1, "deadline": deadline for subtask 2 in unix timestamp, "resources" : [resources needed to accomplish subtask 1], "risks": [risks associated with subtask 1 includes potential roadblocks, obstacles, and challenges that may arise]}, {"name": Subtask 2, "description": Description of subtask 2, , "deadline": deadline for subtask 2 in unix timestamp, "resources": [resources needed to accomplish subtask 2], "risks": [risks associated with subtask 2 includes potential roadblocks, obstacles, and challenges that may arise]}].'''%(plan, complete_before, get_current_datetime())
+    response = llm(prompt)
+    print(response)
+    return Response(content=response, media_type='application/json')
