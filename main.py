c8c3e198ae5294a5f582b446e45556808f5b153b
ghostenginesio <ghostengines.io@gmail.com>
2023-02-19 11:13:59 +0530

    commit

--- main.py
+++ main.py
@@ -35,5 +35,8 @@ async def generate_tasks(plan: str, complete_before: int):
     prompt = '''Please generate a JSON of subtasks and their deadlines for the task %s. The subtasks should be reasonable and not take more than a day to complete. I want to complete all the subtasks in %s days. Current Timestamp: %s
     The JSON output should be in the following format: [{"name": Subtask 1, "description": Description of subtask 1, "deadline": deadline for subtask 2 in unix timestamp, "resources" : [resources needed to accomplish subtask 1], "risks": [risks associated with subtask 1 includes potential roadblocks, obstacles, and challenges that may arise]}, {"name": Subtask 2, "description": Description of subtask 2, , "deadline": deadline for subtask 2 in unix timestamp, "resources": [resources needed to accomplish subtask 2], "risks": [risks associated with subtask 2 includes potential roadblocks, obstacles, and challenges that may arise]}].'''%(plan, complete_before, get_current_datetime())
     response = llm(prompt)
+    headers = {
+        "x-content-type-options": "nosniff"
+    }
     print(response)
-    return Response(content=response, media_type='application/json')
+    return Response(content=response, media_type='application/json', headers=headers)
